47178493e62225d999aea79bbbbe348748540db7
om Podey <84179156+ompodey@users.noreply.github.com>
2021-09-05 13:57:48 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+import cv2
+import time
+import numpy 
+import init_hand_tracking_module      #initial file 
+import mediapipe 
+import math
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
